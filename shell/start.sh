4953eb1c0f53308a4332a547110547bea4cedbc2
chizhang <chizhang@live.cn>
2019-12-22 22:33:18 +0800

    容器

--- shell/start.sh
+++ shell/start.sh
@@ -2,6 +2,7 @@
 source ~/.bashrc
 cd /data/PyWorkspace/DGNebula
 ./project.sh start
+daphne -b 0.0.0.0 -p 18001 DgEt.asgi:application
 #rm -fr /data/var/log/celery/run/worker.pid
 #/etc/init.d/celeryd start
 /bin/bash
